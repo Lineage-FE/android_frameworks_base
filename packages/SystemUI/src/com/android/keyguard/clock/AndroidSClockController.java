a380c7296cbab3ec925d292c7123d1c0cef895de
Chenyang Zhong <zhongcy95@gmail.com>
2021-07-09 06:07:40 -0400

    AndroidSClockController: fix NPE in onAnimationUpdate
    E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.widget.TextClock.setTextSize(float)' on a null object reference
    E AndroidRuntime:	at com.android.keyguard.clock.AndroidSClockController$1.onAnimationUpdate(AndroidSClockController.java:403)
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I868c48be316cd74ff86ee5b59a1dfa50b0f1fcda
    Signed-off-by: DrtSinX98 <pritish@stag-os.org>

--- packages/SystemUI/src/com/android/keyguard/clock/AndroidSClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/AndroidSClockController.java
@@ -415,8 +415,10 @@ public class AndroidSClockController implements ClockPlugin {
                             .setUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                                 @Override
                                 public void onAnimationUpdate(ValueAnimator animation) {
-                                    mClock.setTextSize((float) Converter.dpToPx(mContext, (int) (mTextSizeNormal + (differenceSize * animation.getAnimatedFraction()))));
-                                    mClock.requestLayout();
+                                    if (mClock != null) {
+                                        mClock.setTextSize((float) Converter.dpToPx(mContext, (int) (mTextSizeNormal + (differenceSize * animation.getAnimatedFraction()))));
+                                        mClock.requestLayout();
+                                    }
                                 }
                             })
                             .setDuration(550)
@@ -437,8 +439,10 @@ public class AndroidSClockController implements ClockPlugin {
                             .setUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                                 @Override
                                 public void onAnimationUpdate(ValueAnimator animation) {
-                                    mClock.setTextSize((float) Converter.dpToPx(mContext, (int) (mTextSizeNormal + (differenceSize * (1f - animation.getAnimatedFraction())))));
-                                    mClock.requestLayout();
+                                    if (mClock != null) {
+                                        mClock.setTextSize((float) Converter.dpToPx(mContext, (int) (mTextSizeNormal + (differenceSize * (1f - animation.getAnimatedFraction())))));
+                                        mClock.requestLayout();
+                                    }
                                 }
                             })
                             .setDuration(550)
